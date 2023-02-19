d069de1534aeaa03e15e9b1e4b144e3d609d8a4a
suleiman suleiman <suleimansuleiman@gmail.com>
2023-02-19 17:04:02 +0300

    add intro files

--- reactjs/src/components/intro/Intro.jsx
+++ reactjs/src/components/intro/Intro.jsx
@@ -2,8 +2,22 @@ import "./Intro.scss"
 
 export default function Intro() {
   return (
-    <div id='intro'>
-      Intro
+    <div className="containerIntro" id='intro'>
+      <div className="left">
+        <div className="containerImage">
+          <img src="/assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <p>Hi There, I'm</p>
+        <p>Suleiman Suleiman</p>
+        <p>Freelance <span>MERN developer</span></p>
+        <div className="arrow">
+          <a href="#contact">
+            <img src="/assets/down.png" alt="" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
