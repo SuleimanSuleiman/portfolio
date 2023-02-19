f7dfd2ce7bb593ef69b6637f5466129f4bc2927f
suleiman suleiman <suleimansuleiman@gmail.com>
2023-02-19 18:10:56 +0300

    finished project

--- reactjs/src/components/contact/Contact.jsx
+++ reactjs/src/components/contact/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState } from "react"
+import "./Contact.scss"
 
 export default function Contact() {
+  const [message,setMessage] = useState(false)
   return (
-    <div id='contact'> 
-      Contact
+    <div className='contactContainer' id='contact'> 
+      <div className="left">
+        <img src="/assets/shake.svg" alt="" />
+        </div>
+      <div className="right">
+        <h3>Contact.</h3>
+        <input type="email" placeholder="Email" className="email" />
+        <textarea placeholder="Message"></textarea>
+        <button >Send</button>
+        {(message && <p>Thanks,I'll reply ASAP :)</p>)}
+        </div>
     </div>
   )
 }
